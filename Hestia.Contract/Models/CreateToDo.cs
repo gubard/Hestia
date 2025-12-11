9b9d414833c0bcd174224791da22e3e643a264c5
gubard <maksimovsergey08@gmail.com>
2025-12-11 14:36:29 +0200

    Add `CreateToDo` model and update `HestiaPostRequest` to use it for `Creates`.

---
+++ Hestia.Contract/Models/CreateToDo.cs
@@ -0,0 +1,29 @@
+﻿namespace Hestia.Contract.Models;
+
+public class CreateToDo
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ToDoItemType Type { get; set; }
+    public bool IsBookmark { get; set; }
+    public bool IsFavorite { get; set; }
+    public DateOnly DueDate { get; set; }
+    public TypeOfPeriodicity TypeOfPeriodicity { get; set; }
+    public string AnnuallyDays { get; set; } = string.Empty;
+    public string MonthlyDays { get; set; } = string.Empty;
+    public string WeeklyDays { get; set; } = string.Empty;
+    public ushort DaysOffset { get; set; }
+    public ushort MonthsOffset { get; set; }
+    public ushort WeeksOffset { get; set; }
+    public ushort YearsOffset { get; set; }
+    public ToDoItemChildrenType ChildrenType { get; set; }
+    public string Link { get; set; } = string.Empty;
+    public bool IsRequiredCompleteInDueDate { get; set; }
+    public DescriptionType DescriptionType { get; set; }
+    public string Icon { get; set; } = string.Empty;
+    public string Color { get; set; } = string.Empty;
+    public Guid? ReferenceId { get; set; }
+    public Guid? ParentId { get; set; }
+    public uint RemindDaysBefore { get; set; }
+}
