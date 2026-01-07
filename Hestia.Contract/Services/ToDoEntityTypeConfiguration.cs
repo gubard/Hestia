e560b3128e3536ee1ef857ab87f3263e72e84601
gubard <maksimovsergey08@gmail.com>
2026-01-07 11:54:07 +0200

    Add value comparers for `CreatedDateTime` and `LastCompleted` in `ToDoEntityTypeConfiguration`

--- Hestia.Contract/Services/ToDoEntityTypeConfiguration.cs
+++ Hestia.Contract/Services/ToDoEntityTypeConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Hestia.Contract.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Hestia.Contract.Services;
@@ -14,5 +15,25 @@ public sealed class ToDoEntityTypeConfiguration : IEntityTypeConfiguration<ToDoE
         builder.Property(e => e.NormalizeName).HasMaxLength(255);
         builder.Property(e => e.Description).HasMaxLength(10_000);
         builder.Property(e => e.Link).HasMaxLength(1000);
+
+        builder
+            .Property(e => e.CreatedDateTime)
+            .Metadata.SetValueComparer(
+                new ValueComparer<DateTimeOffset>(
+                    (c1, c2) => c1.Equals(c2),
+                    c => c.GetHashCode(),
+                    c => c
+                )
+            );
+
+        builder
+            .Property(e => e.LastCompleted)
+            .Metadata.SetValueComparer(
+                new ValueComparer<DateTimeOffset?>(
+                    (c1, c2) => c1 == null && c2 == null || c1 != null && c1.Equals(c2),
+                    c => c.GetHashCode(),
+                    c => c
+                )
+            );
     }
 }
