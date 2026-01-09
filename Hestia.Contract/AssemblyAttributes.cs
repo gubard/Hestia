017dd3c2ff2b5a66fa4e34f191d4a1d1e92d48e2
gubard <maksimovsergey08@gmail.com>
2026-01-09 23:37:01 +0200

    Update SqliteAdo registration to disable default behavior

--- Hestia.Contract/AssemblyAttributes.cs
+++ Hestia.Contract/AssemblyAttributes.cs
@@ -1,5 +1,5 @@
 ﻿using Hestia.Contract.Models;
 using Nestor.Db.Models;
 
-[assembly: SqliteAdo(typeof(ToDoEntity), nameof(ToDoEntity.Id))]
+[assembly: SqliteAdo(typeof(ToDoEntity), nameof(ToDoEntity.Id), false)]
 [assembly: SourceEntity(typeof(ToDoEntity), nameof(ToDoEntity.Id))]
