74caba9b92a4ade4073304dd46559b561cf89ebd
gubard <maksimovsergey08@gmail.com>
2026-01-21 15:09:52 +0200

    Add helper method to check if ToDoType has a due date

---
+++ Hestia.Contract/Helpers/ToDoTypeExtension.cs
@@ -0,0 +1,22 @@
+﻿using Hestia.Contract.Models;
+
+namespace Hestia.Contract.Helpers;
+
+public static class ToDoTypeExtension
+{
+    public static bool IsHasDueDate(this ToDoType type)
+    {
+        return type switch
+        {
+            ToDoType.Value => false,
+            ToDoType.Step => false,
+            ToDoType.Circle => false,
+            ToDoType.Group => false,
+            ToDoType.FixedDate => true,
+            ToDoType.Periodicity => true,
+            ToDoType.PeriodicityOffset => true,
+            ToDoType.Reference => false,
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null),
+        };
+    }
+}
